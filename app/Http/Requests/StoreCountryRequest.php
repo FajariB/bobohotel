8517bc05bb558c7c403d4c72ecd9748066bab01e
fajari bagas <fajaribagas09@gmail.com>
2025-08-26 11:27:47 +0700

    add custom form request

---
+++ app/Http/Requests/StoreCountryRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Country;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCountryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255', 'unique'.Country::class], //name harus diisi, string, max 255 karakter, dan unique di table countries kolom name
+        ];
+    }
+}
---
+++ app/Http/Requests/StoreSearchHotelRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreSearchHotelRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'keyword' => ['nullable', 'string', 'max:255'],
+            'checkin_at' => ['nullable', 'date'],
+            'checkout_at' => ['nullable', 'date', 'after:today'],
+        ];  
+    }
+}
